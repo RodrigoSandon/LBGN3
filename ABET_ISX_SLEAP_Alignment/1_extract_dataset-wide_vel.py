095cc766f0f457db01e2ed24fd9a035ff887060c
RodrigoSandon <66277833+RodrigoSandon@users.noreply.github.com>
2022-02-09 16:12:05 -0500

    about to test sleap processing

--- ABET_ISX_SLEAP_Alignment/1_extract_dataset-wide_vel.py
+++ ABET_ISX_SLEAP_Alignment/1_extract_dataset-wide_vel.py
@@ -343,28 +343,57 @@ def export_sleap_data_mult_nodes(h5_filepath, session_root_path, fps):
         track_map_out = f"{name}_tracks.png"
         track_one_node(name, node_loc, track_map_out)
 
-def downsample_algo(df: pd.DataFrame) -> pd.DataFrame:
-    """Every 3 (after including the first one) rows, extract those rows and make a new df."""
-    # if index == 0 or index % 3 == 0 --> keep as new df
-    indices_to_extract = [0]
 
-    for idx, row in df.iterrows():
-        if idx % 3 == 0:
-            indices_to_extract.append(idx)
+def process_speed_data(bodypart: str, root_dir: str, speed_filepath: str):
+    def downsample(speed_filepath) -> pd.DataFrame:
+        """Every 3 (after including the first one) rows, extract those rows and make a new df."""
+        # if index == 0 or index % 3 == 0 --> keep as new df
+        df = pd.read_csv(speed_filepath)
 
-    sub_df = df.iloc[indices_to_extract]
+        indices_to_extract = []
+        for idx, row in df.iterrows():
+            if idx % 3 == 0:
+                indices_to_extract.append(idx)
 
-    return sub_df
+        sub_df = df.iloc[indices_to_extract]
 
+        print(len(sub_df))
 
+        return sub_df
 
-def downsample_speed_data(bodypart, session_root):
-    speed_filepath = os.path.join(session_root, bodypart, f"{bodypart}_sleap_data.csv")
+    def get_time_gpio_starts(gpio_path, stop_at=2) -> float:
+        # at the second time you see "GPIO-1", that's when you choose the "Time (s)" value
+        count = 0
+        gpio_df = pd.read_csv(gpio_path)
 
-    df = pd.read_csv(speed_filepath)
+        for row_idx in range(len(gpio_df)):
+            if "GPIO-1" in gpio_df.loc[row_idx, " Channel Name"]:
+                count += 1
+                if count == stop_at:
+                    time_ABET_starts = gpio_df.loc[row_idx, "Time (s)"]
+                    print(f"GPIO correction: {time_ABET_starts}")
+                    return time_ABET_starts
 
-    sub_df = downsample_algo(df)
-    
+        print("No start time found!")
+
+    def gpio_correct_speed_data(root_dir: str, df: pd.DataFrame) -> pd.DataFrame:
+        """There will be a gpio file in the same dir, jus find the value to add."""
+        gpio_path = os.path.join(root_dir, "gpio.csv")
+
+        gpio_correction = get_time_gpio_starts(gpio_path)  # start time of isx
+        df["idx_time"] = float(df["idx_time"]) + float(gpio_correction)
+
+        return df
+    ##################################################################
+
+    out_path = speed_filepath.replace(bodypart, f"processed_{bodypart}")
+
+    sub_df = downsample(speed_filepath)
+    print(f"length of downsampled df: {len(sub_df)}")
+
+    gpio_corrected_df: pd.DataFrame
+    gpio_corrected_df = gpio_correct_speed_data(sub_df)
+    gpio_corrected_df.to_csv(out_path, index=False)
 
 
 def main():
@@ -388,7 +417,7 @@ def main():
         new_slp_path = os.path.join(ses_root, slp_filename)
         print(f"old path: {i} || new path: {new_slp_path}")
         shutil.move(i, new_slp_path)
-        
+
         # 2) Convert .slp to .h5
         h5_path = new_slp_path.replace(".slp", ".h5")
         slp_to_h5(new_slp_path, h5_path)
@@ -398,9 +427,11 @@ def main():
         export_sleap_data_mult_nodes(h5_path, SESSION_ROOT=ses_root, fps=30)
 
         # 4) Preprocess the speed file
-        downsample_speed_data(bodypart="body", session_root=ses_root)
+        bodypart = "body"
+        speed_filepath = os.path.join(
+            ses_root, bodypart, f"{bodypart}_sleap_data.csv")
 
-
+        process_speed_data(bodypart, ses_root, speed_filepath)
 
     """Other folders will go into a new root folder"""
     """print("===== PROCESSING OTHER FILES =====")
--- ABET_ISX_SLEAP_Alignment/2_preprocess_sleapdata.py
+++ ABET_ISX_SLEAP_Alignment/2_preprocess_sleapdata.py
@@ -2,5 +2,58 @@ import numpy as np
 import pandas as pd
 import os
 
-ex_csv = r"/media/rory/Padlock_DT/Velocity_analysis_example/BLA-Insc-8/RDT_D1/body/body_sleap_data.csv"
+csv = r"/Users/rodrigosandon/Documents/GitHub/LBGN3/SampleData/body_sleap_data.csv"
+root = r"/Users/rodrigosandon/Documents/GitHub/LBGN3/SampleData/"
 
+
+def process_speed_data(bodypart: str, root_dir: str, speed_filepath: str):
+    def downsample(speed_filepath) -> pd.DataFrame:
+        """Every 3 (after including the first one) rows, extract those rows and make a new df."""
+        # if index == 0 or index % 3 == 0 --> keep as new df
+        df = pd.read_csv(speed_filepath)
+
+        indices_to_extract = []
+        for idx, row in df.iterrows():
+            if idx % 3 == 0:
+                indices_to_extract.append(idx)
+
+        sub_df = df.iloc[indices_to_extract]
+
+        return sub_df
+
+    def get_time_gpio_starts(gpio_path, stop_at=2) -> float:
+        # at the second time you see "GPIO-1", that's when you choose the "Time (s)" value
+        count = 0
+        gpio_df = pd.read_csv(gpio_path)
+
+        for row_idx in range(len(gpio_df)):
+            if "GPIO-1" in gpio_df.loc[row_idx, " Channel Name"]:
+                count += 1
+                if count == stop_at:
+                    time_ABET_starts = gpio_df.loc[row_idx, "Time (s)"]
+                    print(f"GPIO correction value: {time_ABET_starts}")
+                    return time_ABET_starts
+
+        print("No start time found!")
+
+    def gpio_correct_speed_data(root_dir: str, df: pd.DataFrame) -> pd.DataFrame:
+        """There will be a gpio file in the same dir, jus find the value to add."""
+        gpio_path = os.path.join(root_dir, "gpio.csv")
+
+        gpio_correction = get_time_gpio_starts(gpio_path)  # start time of isx
+        df["idx_time"] = float(df["idx_time"]) + float(gpio_correction)
+
+        return df
+    ##################################################################
+
+    out_path = speed_filepath.replace(bodypart, f"processed_{bodypart}")
+
+    sub_df = downsample(speed_filepath)
+    print(f"length of downsampled df: {len(sub_df)}")
+
+    gpio_corrected_df: pd.DataFrame
+    gpio_corrected_df = gpio_correct_speed_data(root_dir, sub_df)
+    gpio_corrected_df.to_csv(out_path, index=False)
+
+
+process_speed_data("body", root, csv)
