826fb95049b6ee2790b08488dc2dcc467053a56d
RodrigoSandon <rodrigosandon@gmail.com>
2022-03-01 19:47:03 -0500

    rory improved model

--- Decoding/RunDifferentClassifiers.py
+++ Decoding/RunDifferentClassifiers.py
@@ -10,17 +10,22 @@ d = {
 
 """
 import os, glob
-from tarfile import BLOCKSIZE
 import pandas as pd
 from pathlib import Path
 from typing import List, Tuple
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
+from sklearn.decomposition import PCA
+from sklearn.metrics import f1_score, confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
 from sklearn.svm import SVC
 
 
@@ -36,56 +41,138 @@ def dissect_outcome(path: Path):
     return list(path.parts)[8]
 
 
-# /media/rory/Padlock_DT/BLA_Analysis/Decoding/Arranged_Dataset/{1.0}/[Large]/{BLA-Insc-1}/{RDT D1}/[trail_1.csv]
-def binary_logreg():
-    ROOT_PATH = Path(r"/media/rory/Padlock_DT/BLA_Analysis/Decoding/Arranged_Dataset/")
-    block = "1.0"
-    session = "RDT D1"
-    mouse = "BLA-Insc-1"
+def get_pipe_w_f1_results(pipe, X_train, X_test, y_train, y_test) -> dict:
+    f1_scores = cross_val_score(pipe, X_train, y_train, scoring="f1_weighted")
+    avg_val_f1 = np.average(f1_scores)
+    # print("Average Cross-Validation F1 Score: ", avg_f1)
 
-    files = find_paths(ROOT_PATH, block, mouse, session, "trail")
+    pipe.fit(X_train, y_train)
+    weighted_prediction = pipe.predict(X_test)
+    w_f1 = f1_score(y_test, weighted_prediction, average="weighted")
+    # print(f"Weighted F1 Score: {w_f1}")
+    plot_confusion_matrix(y_test, weighted_prediction, labels=["Large", "Small"])
 
-    # print(*files, sep="\n")
-    print("Number of trials (csvs): ", len(files))
+    return {"val f1": round(avg_val_f1, 3), "test f1": round(w_f1, 3)}
 
-    y = []
-    X = []
 
-    for csv in files:
-        csv = Path(csv)
-        outcome = dissect_outcome(csv)
-        df: pd.DataFrame
-        df = pd.read_csv(csv)
-        df = df.T
-        df = df.iloc[1:, :]  # remove first row (the cell names)
-        # go through columns and add to X and y
-        for col in list(df.columns):
-            X.append(list(df[col]))
-            y.append(outcome)
+def plot_confusion_matrix(y_true, y_pred, labels):
+    cf_matrix = confusion_matrix(y_true, y_pred, labels).astype(int)
+    ax = sns.heatmap(cf_matrix, annot=True, cmap="Blues")
+    ax.set_title("Actual Outcome vs. Predicted Outcome")
+    ax.set_xlabel("\nPredicted Outcomes")
+    ax.set_ylabel("Actual Outcomes")
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.20, train_size=0.80, random_state=0
-    )
+    ax.xaxis.set_ticklabels(["Large", "Small"])
+    ax.yaxis.set_ticklabels(["Large", "Small"])
 
-    print("Number of samples: ", len(X_train))
-    # print(f"y : {y}")
+    plt.show()
 
+
+def logistic_regression(X_train, X_test, y_train, y_test, results: dict) -> dict:
     # l2 -> all neurons make small contributions
     # l1 -> only some neurons make large contributions
-    model_pipe = make_pipeline(
-        StandardScaler(), LogisticRegression(penalty="l2", max_iter=200)
+    # pipe = make_pipeline(
+    # StandardScaler(), LogisticRegression(penalty="l2", max_iter=400),
+    # )
+    # clf = LogisticRegression(
+    #     penalty="l2",
+    #     # l1_ratio=0,
+    #     C=50,
+    #     # solver="saga",
+    #     max_iter=500,
+    #     class_weight={"Large": 0.2, "Small": 0.4},
+    # )
+    # clf = RandomForestClassifier(n_estimators=30)
+    clf = LinearDiscriminantAnalysis()
+
+    pipe = make_pipeline(StandardScaler(), clf)
+
+    results["logistic_reg"] = get_pipe_w_f1_results(
+        pipe, X_train, X_test, y_train, y_test
     )
 
-    # to record scores across multiple classifiers
-    model_results = {}
-    f1_scores = cross_val_score(model_pipe, X_train, y_train, scoring="f1_macro")
-    avg_f1 = np.average(f1_scores)
-    print("Average F1 Score: ", avg_f1)
-    # print(type(f1_scores))
+    return results
 
-    model_pipe.fit(X_train, y_train)
-    print("Model accuracy: %.3f" % model_pipe.score(X_test, y_test))
 
+def svm_svc(X_train, X_test, y_train, y_test, results: dict) -> dict:
+    # l2 -> all neurons make small contributions
+    # l1 -> only some neurons make large contributions
+    pipe = make_pipeline(StandardScaler(), SVC())
+
+    results["SVC"] = get_pipe_w_f1_results(pipe, X_train, X_test, y_train, y_test)
+
+    return results
+
+
+def gaussian_NB(X_train, X_test, y_train, y_test, results: dict) -> dict:
+    pipe = make_pipeline(StandardScaler(), PCA(3), GaussianNB())
+
+    results["gaussian_nb"] = get_pipe_w_f1_results(
+        pipe, X_train, X_test, y_train, y_test
+    )
+
+    return results
+
+
+# /media/rory/Padlock_DT/BLA_Analysis/Decoding/Arranged_Dataset/{1.0}/[Large]/{BLA-Insc-1}/{RDT D1}/[trail_1.csv]
+def binary_classifications():
+    ROOT_PATH = Path(r"/media/rory/Padlock_DT/BLA_Analysis/Decoding/Arranged_Dataset/")
+    session = "RDT D1"
+    mouse = "BLA-Insc-1"
+    blocks = ["1.0", "2.0", "3.0"]
+
+    for i, block in enumerate(blocks):
+        if i != 2:
+            continue
+
+        print()
+        print(f"PREDICTING OUTCOME IN BLOCK {block}, {mouse} {session}")
+        files = find_paths(ROOT_PATH, block, mouse, session, "trail")
+        files_small_large = [i for i in files if not "Omission" in i and not "ITI" in i]
+        # print(*files, sep="\n")
+        print("Number of trials (csvs): ", len(files_small_large))
+
+        y = []
+        X = []
+        # to record scores across multiple classifiers
+        f1_results = {}
+
+        for csv in files_small_large:
+            csv = Path(csv)
+            outcome = dissect_outcome(csv)
+            df: pd.DataFrame
+            df = pd.read_csv(csv)
+            df = df.T
+            df = df.iloc[1:, :]  # remove first row (the cell names)
+            # go through columns and add to X and y
+            for col in list(df.columns):
+                X.append(list(df[col]))
+                y.append(outcome)
+
+        print("Number of cells: ", len(X))
+        # pull only unique elements in list
+        labels = []
+        for ele in y:
+            if ele not in labels:
+                labels.append(ele)
+        print(f"Labels: {labels}")
+
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.20, train_size=0.80, random_state=None
+        )
+
+        ######### INPUT CLASSIFIERS HERE #########
+        f1_results = logistic_regression(X_train, X_test, y_train, y_test, f1_results)
+        # f1_results = gaussian_NB(X_train, X_test, y_train, y_test, f1_results)
+        # f1_results = svm_svc(X_train, X_test, y_train, y_test, f1_results)
+
+        ######### PRINT RESULTS #########
+        for key, val in f1_results.items():
+            print(key, ":", val)
+
+
+if __name__ == "__main__":
+    binary_classifications()
 
 """def load_data() -> Tuple[np.ndarray, np.ndarray]:
 
@@ -112,67 +199,3 @@ results["logistc"] = cross_val_score(logistic_pipe, X, y, scor)
 svm
 
 # model = make_pipeline(StandardScaler(), LogisticRegression())"""
-
-
-def multinomial_logreg():
-    ROOT_PATH = Path(r"/media/rory/Padlock_DT/BLA_Analysis/Decoding/Arranged_Dataset/")
-    session = "RDT D1"
-    mouse = "BLA-Insc-1"
-    blocks = ["1.0", "2.0", "3.0"]
-    for block in blocks:
-        print()
-        print(f"PREDICTING OUTCOME IN BLOCK {block}, {mouse} {session}")
-        files = find_paths(ROOT_PATH, block, mouse, session, "trail")
-        files_small_large = [i for i in files if not "Omission" in i and not "ITI" in i]
-        # print(*files, sep="\n")
-        print("Number of trials (csvs): ", len(files_small_large))
-
-        y = []
-        X = []
-
-        for csv in files_small_large:
-            csv = Path(csv)
-            outcome = dissect_outcome(csv)
-            df: pd.DataFrame
-            df = pd.read_csv(csv)
-            df = df.T
-            df = df.iloc[1:, :]  # remove first row (the cell names)
-            # go through columns and add to X and y
-            for col in list(df.columns):
-                X.append(list(df[col]))
-                y.append(outcome)
-
-        # pull only unique elements in list
-        labels = []
-        for ele in y:
-            if ele not in labels:
-                labels.append(ele)
-        print(f"Labels: {labels}")
-
-        X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size=0.20, train_size=0.80, random_state=0
-        )
-
-        print("Number of samples: ", len(X_train))
-        # print(f"y : {y}")
-
-        # l2 -> all neurons make small contributions
-        # l1 -> only some neurons make large contributions
-        model_pipe = make_pipeline(
-            StandardScaler(),
-            LogisticRegression(penalty="l2", max_iter=400, multi_class="multinomial"),
-        )
-
-        # to record scores across multiple classifiers
-        model_results = {}
-        f1_scores = cross_val_score(model_pipe, X_train, y_train, scoring="f1_macro")
-        avg_f1 = np.average(f1_scores)
-        print("Average F1 Score: ", avg_f1)
-        # print(type(f1_scores))
-
-        model_pipe.fit(X_train, y_train)
-        print("Model accuracy: %.3f" % model_pipe.score(X_test, y_test))
-
-
-if __name__ == "__main__":
-    multinomial_logreg()
