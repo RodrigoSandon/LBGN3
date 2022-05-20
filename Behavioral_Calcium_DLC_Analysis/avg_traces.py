3a96d10d3a9b83e34a34f451834f3a4189c1a9a1
RodrigoSandon <rodrigosandon@gmail.com>
2022-05-20 15:49:16 -0400

    changes to concat fig

---
+++ Behavioral_Calcium_DLC_Analysis/avg_traces.py
@@ -0,0 +1,60 @@
+import os, glob
+from pathlib import Path
+import pandas as pd
+from scipy import stats
+
+def find_paths_endswith(root_path, endswith) -> list:
+
+    files = glob.glob(
+        os.path.join(root_path,"**", "%s") % (endswith), recursive=True,
+    )
+
+    return files
+
+def main1():
+    # /media/rory/Padlock_DT/BLA_Analysis/BetweenMiceAlignmentData/RDT D1/Block_Reward Size_Choice Time (s)/(1.0, 'Large')/sorted_traces_z_fullwindow_id_auc_bonf0.05.csv
+
+    ROOT = "/media/rory/Padlock_DT/BLA_Analysis/BetweenMiceAlignmentData"
+
+    files_to_avg = find_paths_endswith(ROOT, "sorted_traces_z_fullwindow_id_auc_bonf0.05.csv")
+
+    #len is # of cells
+    list_of_traces = []
+    for file in files_to_avg:
+        print(f"CURR FILE: {file}")
+        root = Path(file).parent
+        df = pd.read_csv(file)
+
+        zipped = zip(*[list(df["+_mean"]), list(df["-_mean"]), list(df["Neutral_mean"])])
+
+        avg = [(x + y + z)/3 for x,y,z in zipped]
+
+        avg_df = pd.DataFrame(avg, index = None, columns=["Avg dff trace"])
+        avg_df.to_csv(os.path.join(root, "avg_dff_trace.csv"), index=False)
+
+def main2():
+    # /media/rory/Padlock_DT/BLA_Analysis/BetweenMiceAlignmentData/RDT D1/Block_Reward Size_Choice Time (s)/(1.0, 'Large')/sorted_traces_z_fullwindow_id_auc_bonf0.05.csv
+
+    ROOT = "/media/rory/Padlock_DT/BLA_Analysis/BetweenMiceAlignmentData"
+
+    files_to_avg = find_paths_endswith(ROOT, "all_concat_cells.csv")
+
+    #len is # of cells
+    for file in files_to_avg:
+        print(f"CURR FILE: {file}")
+        root = Path(file).parent
+        df = pd.read_csv(file)
+        list_of_traces = []
+ 
+        for col in list(df.columns):
+            list_of_traces.append(list(df[col]))
+
+        zipped = zip(*list_of_traces)
+        avg = stats.zscore([sum(list(tup))/len(list(tup)) for tup in zipped])
+
+        avg_df = pd.DataFrame(avg, index = None, columns=["Avg dff trace"])
+        avg_df.to_csv(os.path.join(root, "avg_dff_trace.csv"), index=False)
+
+
+if __name__ == "__main__":
+    main2()
--- SLEAP/combining_other_figures/using_gridspec.py
+++ SLEAP/combining_other_figures/using_gridspec.py
@@ -6,6 +6,7 @@ import pandas as pd
 from statistics import mean
 from operator import attrgetter
 import seaborn as sns
+from scipy import stats
 
 class Cell:
     def __init__(self,cell_name, dff_trace: list):
@@ -59,32 +60,9 @@ def get_min_of_df(df: pd.DataFrame):
  
     return global_min
 
-hm_df_2 = pd.read_csv("/media/rory/Padlock_DT/BLA_Analysis/BetweenMiceAlignmentData/RDT D1/Block_Reward Size_Choice Time (s)/(2.0, 'Large')/all_concat_cells_z_fullwindow.csv")
-hm_df_sort_2 = sort_cells(hm_df_2)
-speed_df_2 = pd.read_csv("/media/rory/Padlock_DT/BLA_Analysis/BetweenMiceAlignmentData/RDT D1/Block_Reward Size_Choice Time (s)/(2.0, 'Large')/avg_unnorm_speed.csv")
-hm_yticks_2 = list(hm_df_sort_2.columns)
-speed_list_2 = list(speed_df_2["Avg. Speed (cm/s)"])
-speed_max_2 = round(max(list(speed_df_2["Avg. Speed (cm/s)"])), 1)
-speed_min_2 = round(min(list(speed_df_2["Avg. Speed (cm/s)"])), 1)
-speed_yticks_2 = np.arange(speed_min_2, speed_max_2, 1.5)
-hm_df_matrix_2 = hm_df_sort_2.to_numpy().T
-
-hm_df = pd.read_csv("/media/rory/Padlock_DT/BLA_Analysis/BetweenMiceAlignmentData/RDT D1/Block_Reward Size_Choice Time (s)/(1.0, 'Large')/all_concat_cells_z_fullwindow.csv")
-hm_df_sort = sort_cells(hm_df)
-speed_df = pd.read_csv("/media/rory/Padlock_DT/BLA_Analysis/BetweenMiceAlignmentData/RDT D1/Block_Reward Size_Choice Time (s)/(1.0, 'Large')/avg_unnorm_speed.csv")
-hm_yticks = list(hm_df_sort.columns)
-speed_list = list(speed_df["Avg. Speed (cm/s)"])
-speed_max = round(max(list(speed_df["Avg. Speed (cm/s)"])), 1)
-speed_min = round(min(list(speed_df["Avg. Speed (cm/s)"])), 1)
-speed_yticks = np.arange(speed_min, speed_max, 1.5)
-hm_df_matrix = hm_df_sort.to_numpy().T
-t_pos = np.arange(0.0, 10.0, 0.1)
-t_neg = np.arange(-10.0, 0.0, 0.1)
-t = t_neg.tolist() + t_pos.tolist()
-t_ticks = np.arange(0, len(t), 1)
-
-
+event = "Block_Reward Size_Choice Time (s)"
 subevents = ["(1.0, 'Large')", "(2.0, 'Large')", "(3.0, 'Large')"]
+name_of_png = f"all_blocks_{event}_Large_hm_speed_avg_trace.png"
 
 fig = plt.figure(figsize=(15, 8))
 outer = gridspec.GridSpec(1, len(subevents), wspace=0.0, hspace=0.0)
@@ -96,21 +74,23 @@ for idx, subevent in enumerate(subevents):
     # of the same subevent
     hm_df = pd.read_csv(f"/media/rory/Padlock_DT/BLA_Analysis/BetweenMiceAlignmentData/RDT D1/Block_Reward Size_Choice Time (s)/{subevent}/all_concat_cells_z_fullwindow.csv")
     speed_df = pd.read_csv(f"/media/rory/Padlock_DT/BLA_Analysis/BetweenMiceAlignmentData/RDT D1/Block_Reward Size_Choice Time (s)/{subevent}/avg_unnorm_speed.csv")
-    avg_trace_df = pd.read_csv(f"/media/rory/Padlock_DT/BLA_Analysis/BetweenMiceAlignmentData/RDT D1/Block_Reward Size_Choice Time (s)/{subevent}/sorted_traces_z_fullwindow_id_auc.csv")
+    avg_trace_df = pd.read_csv(f"/media/rory/Padlock_DT/BLA_Analysis/BetweenMiceAlignmentData/RDT D1/Block_Reward Size_Choice Time (s)/{subevent}/avg_dff_trace.csv")
     hm_df_sort = sort_cells(hm_df)
     hm_yticks = list(hm_df_sort.columns)
-    speed_list = list(speed_df["Avg. Speed (cm/s)"])
-    speed_max = round(max(list(speed_df["Avg. Speed (cm/s)"])), 1)
-    speed_min = round(min(list(speed_df["Avg. Speed (cm/s)"])), 1)
-    speed_yticks = np.arange(speed_min, speed_max, 1.5)
+    speed_list = stats.zscore(list(speed_df["Avg. Speed (cm/s)"])) # z scored
+    avg_dff_list = list(avg_trace_df["Avg dff trace"]) # already z-scored (avg zscores)
+    max_val = round(max(speed_list), 0)
+    min_val = round(min(speed_list + avg_dff_list), 0)
+    speed_yticks = np.arange(min_val, max_val + 1, 1)
     hm_df_matrix = hm_df_sort.to_numpy().T
     t_pos = np.arange(0.0, 10.0, 0.1)
     t_neg = np.arange(-10.0, 0.0, 0.1)
     t = t_neg.tolist() + t_pos.tolist()
-    t_ticks = np.arange(0, len(t), 1)
+    t = [round(i, 1) for i in t]
 
     if idx > 0:
         ax_1 = plt.Subplot(fig, inner[0])
+        ax_1.set_title(f"Block {idx + 1}")
         ax_1.pcolormesh(hm_df_matrix, vmin = get_min_of_df(hm_df_sort), vmax = get_max_of_df(hm_df_sort), cmap="bwr")
         ax_1.set_yticks([])
         ax_1.set_xticks([])
@@ -119,14 +99,17 @@ for idx, subevent in enumerate(subevents):
         fig.add_subplot(ax_1)
 
         ax_2 = plt.Subplot(fig, inner[1])
-        ax_2.plot(t_ticks, speed_list)
+        ax_2.plot(t, speed_list, label="Norm. Speed (cm/s)")
+        ax_2.plot(t, avg_dff_list, c="indianred", label="Norm. Avg. DF/F")
         ax_2.set_yticks([])
-        ax_2.set_ylim(0, speed_max + 1)
-        ax_2.set_xlabel("Time Relative to Choice (s)")
+        ax_2.set_ylim(min_val, max_val + 1)
+        if idx == 1:
+            ax_2.set_xlabel("Time Relative to Choice (s)")
 
         fig.add_subplot(ax_2)
     else:   
         ax_1 = plt.Subplot(fig, inner[0])
+        ax_1.set_title(f"Block {idx + 1}")
         ax_1.pcolormesh(hm_df_matrix, vmin = get_min_of_df(hm_df_sort), vmax = get_max_of_df(hm_df_sort), cmap="bwr")
         ax_1.set_yticks(range(0, len(hm_yticks)), hm_yticks)
         ax_1.set_xticks([])
@@ -136,12 +119,14 @@ for idx, subevent in enumerate(subevents):
         fig.add_subplot(ax_1)
 
         ax_2 = plt.Subplot(fig, inner[1])
-        ax_2.plot(t_ticks, speed_list)
+        ax_2.plot(t, speed_list, label="Norm. Speed (cm/s)")
+        ax_2.plot(t, avg_dff_list, c="indianred", label="Norm. Avg. DF/F")
+        ax_2.legend()
         ax_2.set_yticks(speed_yticks)
-        ax_2.set_ylim(0, speed_max + 1)
-        ax_2.set_ylabel("Speed (cm/s)")
-        ax_2.set_xlabel("Time Relative to Choice (s)")
+        ax_2.set_ylim(min_val, max_val + 1)
+        ax_2.set_ylabel("Z-score")
 
         fig.add_subplot(ax_2)
 
-plt.savefig(f"/media/rory/Padlock_DT/BLA_Analysis/Results/all_blocks_L_hm_speed_avg_trace.png")
+# to have proper time period, just insert the -10 to 10 into x axis 
+plt.savefig(f"/media/rory/Padlock_DT/BLA_Analysis/Results/{name_of_png}")
